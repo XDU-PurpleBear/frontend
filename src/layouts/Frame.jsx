9df7f3b8f042ad4d4e9d878189b0f94c6ed2c120
JeyserYang <32164278+JeyserYang@users.noreply.github.com>
2017-11-03 18:15:37 +0800

    Add files via upload

--- src/layouts/Frame.jsx
+++ src/layouts/Frame.jsx
@@ -6,72 +6,60 @@ const { Header, Sider, Content } = Layout;
 
 import styles from "./Frame.scss";
 
-import BookSearch from "../views/BookSearch.jsx";
-import UserCenter from "../views/UserCenter.jsx";
+import SearchForm from "../components/Frame/SearchForm.jsx";
+import UserEntry from "../components/Frame/UserEntry.jsx";
+// import UserCenter from "../views/UserCenter.jsx";
 import Main from "../views/Main.jsx";
+import BookSearchReault from "../views/BookSearchResult.jsx";
 import Detail from "../views/Detail.jsx";
+import AddBook from "../components/BookManagement/AddBook.jsx";
+import EditBook from "../components/BookManagement/EditBook.jsx";
+import BookApply from "../components/UserCenter/BookApply.jsx";
+import BookBorrow from "../components/UserCenter/BookBorrow.jsx";
+import BookReturn from "../components/UserCenter/BookReturn.jsx";
+import AdminInfo from "../components/UserCenter/AdminInfo.jsx";
+import ReaderInfo from "../components/UserCenter/ReaderInfo.jsx";
+
 import NotFound from "../views/NotFound.jsx";
 
 @connect(state => {
     return {
-        ...(state.UserCenter.default.userType),
+        userType: state.UserCenter.default.userType,
     };
 })
 class Frame extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            collapsed: true,
-        }
-        this.toggle = this.toggle.bind(this);
-    }
-    toggle() {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
     }
     render() {
-        const defaultSelectedKeys = [this.props.location.pathname];
-        const iconStyle = {
-            fontSize:"x-large",
-            width: "100%",
-            height: "100%",
-            color: "white",
-            marginTop: "5px",
-        };
+        const {history} = this.props;
         return (
             <div className={styles.frame}>
                 <Layout className={styles.navLayout}>
-                    <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                        <div theme="dark" className={styles.navLayoutToggle} onClick={this.toggle}>
-                            <Icon style={iconStyle} type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}  />
-                        </div>
-                        <Menu theme="dark" mode="inline" defaultSelectedKeys={defaultSelectedKeys}>
-                            <Menu.Item key="/booksearch">
-                                <Link to="/booksearch">
-                                    <Icon type="search" />
-                                    <span>Search Book</span>
-                                </Link>                                                                    
-                            </Menu.Item>
-                            <Menu.Item key="/usercenter">
-                                <Link to="/usercenter">
-                                    <Icon type="user" />
-                                    <span>User Center</span>
-                                </Link>                                
-                            </Menu.Item>
+                    <Header className={styles.navHeader}>
+                        <span className={styles.title}>XDU Library</span>
+                        <SearchForm history={history} />
+                        <UserEntry className={styles.UserEntry}/>
+                    </Header>
+                    <Content className={styles.navContent}>
+                        <Switch>
+                            <Route exact path="/" component={Main}/>
+                            <Route exact path="/booksearchresult/:searchType/:searchValue" component={BookSearchReault}/>
+                            <Route exact path="/detail/:ISBN" component={Detail}/>
 
-                        </Menu>
-                    </Sider>
-                    <Layout>
-                        <Content className={styles.navContent}>
-                            <Switch>
-                                <Route exact path="/" component={Main}/>
-                                <Route path="/booksearch" component={BookSearch}/>
-                                <Route path="/usercenter" component={UserCenter}/>
-                                <Route component={NotFound} />
-                            </Switch>
-                        </Content>
-                    </Layout>
+                            <Route exact path="/bookmanagement/add" component={AddBook}/>
+                            <Route exact path="/bookmanagement/edit" component={EditBook}/>
+
+                            <Route path="/usercenter/admininfo" component={AdminInfo}/> 
+                            <Route path="/usercenter/readerinfo" component={ReaderInfo}/>
+
+                            <Route path="/usercenter/bookapply" component={BookApply}/>
+                            <Route path="/usercenter/bookreturn" component={BookReturn}/>
+                            <Route path="/usercenter/bookborrow" component={BookBorrow}/>
+
+                            <Route component={NotFound} />
+                        </Switch>
+                    </Content>
                 </Layout>
             </div>
         );
